cc48203d1982559fad6c072fbdb758f5e2279ddb
andy5995 <andy400-dev@yahoo.com>
2018-08-23 09:31:25 -0500

    narrow scope of faces array (#1)

--- src/aa-pokerhands.c
+++ src/aa-pokerhands.c
@@ -42,20 +42,20 @@ int main(int argc, char *argv[]) {
   "Clubs   "
   };
 
-  char *faces[] = {
-  "Ace",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
-  "10",
-  "Jack",
-  "Queen",
-  "King"
+  const char *faces[] = {
+    "Ace",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "Jack",
+    "Queen",
+    "King"
   };
 
   high_straight = 0;
